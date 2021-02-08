5eda6c83aa02931ddab40e37dea80142a9484a2a
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-08 23:22:04 +0300

    Implement VirtualMachine.cs file

--- VirtualMachine.cs
+++ VirtualMachine.cs
@@ -7,20 +7,32 @@ namespace func.brainfuck
 	{
 		public VirtualMachine(string program, int memorySize)
 		{
+			Instructions = program;
+			Memory = new byte[memorySize];
+			InstructionPointer = 0;
+			MemoryPointer = 0;
+			InstructionSet = new Dictionary<char, Action<IVirtualMachine>>();
 		}
 
 		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
 		{
-			throw new NotImplementedException();
+			InstructionSet.Add(symbol, execute);
 		}
 
+		protected Dictionary<char, Action<IVirtualMachine>> InstructionSet { get; set; }
 		public string Instructions { get; }
 		public int InstructionPointer { get; set; }
 		public byte[] Memory { get; }
 		public int MemoryPointer { get; set; }
+
 		public void Run()
 		{
-			throw new NotImplementedException();
+			while (InstructionPointer < Instructions.Length)
+			{
+				if (InstructionSet.TryGetValue(Instructions[InstructionPointer], out var action))
+					action(this);
+				InstructionPointer++;
+			}
 		}
 	}
 }
