b794f3e42e1e16b8a2ed30e62587193bcbc66bb2
Nikita Minyuk <nikita.minyuk.w@gmail.com>
2021-02-08 23:25:18 +0300

    Implement BrainfuckLoopCommands.cs file

--- BrainfuckLoopCommands.cs
+++ BrainfuckLoopCommands.cs
@@ -6,8 +6,34 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm)
 		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
+			Dictionary<int, int> loopsByStart = new Dictionary<int, int>();
+			Stack<int> currentLoopsByStart = new Stack<int>();
+			vm.RegisterCommand('[', b =>
+			{
+				if (loopsByStart.Count == 0)
+				{
+					Stack<int> startOfLoops = new Stack<int>();
+					startOfLoops.Push(b.InstructionPointer);
+					for (int tempIndex = b.InstructionPointer + 1; tempIndex != b.Instructions.Length; tempIndex++)
+					{
+						if (b.Instructions[tempIndex] == '[')
+							startOfLoops.Push(tempIndex);
+						else if (b.Instructions[tempIndex] == ']')
+							loopsByStart[startOfLoops.Pop()] = tempIndex;
+					}
+				}
+				if (b.Memory[b.MemoryPointer] == 0)
+					b.InstructionPointer = loopsByStart[b.InstructionPointer];
+				else
+					currentLoopsByStart.Push(b.InstructionPointer);
+			});
+			vm.RegisterCommand(']', b =>
+			{
+				if (b.Memory[b.MemoryPointer] != 0)
+					b.InstructionPointer = currentLoopsByStart.Peek();
+				else
+					currentLoopsByStart.Pop();
+			});
 		}
 	}
 }
